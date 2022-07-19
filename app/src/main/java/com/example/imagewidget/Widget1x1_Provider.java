829cd8a76cf0c18582c0e513c25f37ad24994398
gofogo2 <promoted.go@gmail.com>
2022-07-19 14:34:15 +0900

    feat:최종 업데이트

--- app/src/main/java/com/example/imagewidget/Widget1x1_Provider.java
+++ app/src/main/java/com/example/imagewidget/Widget1x1_Provider.java
@@ -1,9 +1,12 @@
 package com.example.imagewidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -17,40 +20,46 @@ public class Widget1x1_Provider  extends AppWidgetProvider {
     Integer cnt =0;
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-            ComponentName widget = new ComponentName(context,Widget1x1_Provider.class);
-            for (int appWidgetId : appWidgetIds) {
-            RemoteViews views  = new RemoteViews(context.getPackageName(), R.layout.widget1x1__provider);
-            TimerTask timerTask = new TimerTask() {
-                @Override
-                public void run() {
-                    cnt++;
-                    cnt=cnt%5;
+        super.onUpdate(context,appWidgetManager,appWidgetIds);
+//        ComponentName widget = new ComponentName(context,Widget1x1_Provider.class);
+        RemoteViews views  = new RemoteViews(context.getPackageName(), R.layout.widget1x1__provider);
+            Intent intent=new Intent(context, MainActivity.class);
 
-                    switch (cnt){
-                        case 1:
-                            views.setImageViewResource(R.id.img1,R.drawable.img_1x1_1);
-                            break;
-                        case 2:
-                            views.setImageViewResource(R.id.img1,R.drawable.img_1x1_2);
-                            break;
-                        case 3:
-                            views.setImageViewResource(R.id.img1,R.drawable.img_1x1_3);
-                            break;
-                        case 4:
-                            views.setImageViewResource(R.id.img1,R.drawable.img_1x1_4);
-                            break;
-                        case 5:
-                            views.setImageViewResource(R.id.img1,R.drawable.img_1x1_5);
-                            break;
-                        default:
-                            break;
-                    }
-                    appWidgetManager.updateAppWidget(widget,views);
-                }
-            };
+            PendingIntent pe=PendingIntent.getActivity(context, 0, intent, 0);
 
-            Timer timer = new Timer();
-            timer.schedule(timerTask,0,3000);
-        }
+
+
+            appWidgetManager.updateAppWidget(appWidgetIds,views);
+
+
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+        ComponentName widget = new ComponentName(context,Widget1x1_Provider.class);
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+        RemoteViews views  = new RemoteViews(context.getPackageName(), R.layout.widget1x1__provider);
+        views.setImageViewResource(R.id.img1,R.drawable.img_1x1_4);
+        appWidgetManager.updateAppWidget(widget,views);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        Log.d("aaaaaa","onEnabled");
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onRestored(Context context, int[] oldWidgetIds, int[] newWidgetIds) {
+        Log.d("aaaaaa","onRestored");
+        super.onRestored(context, oldWidgetIds, newWidgetIds);
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        Log.d("aaaaaa","onAppWidgetOptionsChanged");
+        super.onAppWidgetOptionsChanged(context, appWidgetManager, appWidgetId, newOptions);
     }
 }
--- app/src/main/java/com/example/imagewidget/Widget3x1_Provider.java
+++ app/src/main/java/com/example/imagewidget/Widget3x1_Provider.java
@@ -1,8 +1,10 @@
 package com.example.imagewidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 /**
@@ -10,9 +12,12 @@ import android.widget.RemoteViews;
  */
 public class Widget3x1_Provider extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget3x1__provider);
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
+//    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+//                                int appWidgetId) {
+//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget3x1__provider);
+//        Intent intent=new Intent(context, MainActivity.class);
+//        PendingIntent pe= PendingIntent.getActivity(context, 0, intent, 0);
+//        views.setOnClickPendingIntent(R.id.llRoot, pe);
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
+//    }
 }
